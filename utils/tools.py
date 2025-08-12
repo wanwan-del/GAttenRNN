1761a1cef31e002e078c10246d59047d39bbd09b
wanwan-del <1079424384@qq.com>
2025-08-12 16:32:11 +0800

    Add files via upload

---
+++ utils/tools.py
@@ -0,0 +1,43 @@
+import argparse
+
+import numpy as np
+import torch
+
+
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
+def inverse_permutation(perm):
+    inv = torch.empty_like(perm)
+    inv[perm] = torch.arange(perm.size(0), device=perm.device)
+    return inv
+
+
+def get_sinusoid_encoding_table(positions, d_hid, T=1000):
+    """Sinusoid position encoding table
+    positions: int or list of integer, if int range(positions)"""
+
+    if isinstance(positions, int):
+        positions = list(range(positions))
+
+    def cal_angle(position, hid_idx):
+        return position / np.power(T, 2 * (hid_idx // 2) / d_hid)
+
+    def get_posi_angle_vec(position):
+        return [cal_angle(position, hid_j) for hid_j in range(d_hid)]
+
+    sinusoid_table = np.array([get_posi_angle_vec(pos_i) for pos_i in positions])
+
+    sinusoid_table[:, 0::2] = np.sin(sinusoid_table[:, 0::2])  # dim 2i
+    sinusoid_table[:, 1::2] = np.cos(sinusoid_table[:, 1::2])  # dim 2i+1
+
+    return torch.FloatTensor(sinusoid_table)
+
---
+++ utils/visualizations/vision_selection.py
@@ -0,0 +1,5 @@
+from task.spatiotemporal.visualization import vision_minist
+
+Visualization = {
+    "moving_mnist": vision_minist,
+}
